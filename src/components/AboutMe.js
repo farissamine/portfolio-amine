284b58b5508a09af15fe5920e9ed3112052eb200
Amine Aboufariss <farissamine99@gmail.com>
2021-05-03 19:09:18 -0500

    Add Experience

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -14,9 +14,9 @@ const AboutMe = () => {
                     <h1 class ="about-heading">about me</h1>
                     <p>
                         Hello, I am Amine. I have been developing software professionally for about 2 years. I am a Full-Stack developer and I mostly enjoy working on back end systems. I find enjoyment in immediately seeing the results of my code in robotic functions. I also aspire to develop code that can be aidful to those in the medical industry.
-
-                        I am an experienced Software Engineer with a demonstrated history of working in the law and industrial automation industry. I am skilled in Java, Python, C++, JavaScript and a few other languages. Strong engineering professional with a Bachelor's degree focused in Computer Science from DePaul University.
-
+                        <br /><br />
+                        I am an experienced Software Engineer with a demonstrated history of working in the law and industrial automation industry. Skilled in Java, Python, C++, JavaScript and a few other languages, I am a strong engineering professional with a Bachelor's degree focused in Computer Science from DePaul University during the year of 2020.
+                        <br /><br />
                         My experience as a professional programmer for the past 2 years, ability to learn languages quickly and effectively, and love for coding in my free time with an eagerness to learn, make me an excellent software developer. 
                     </p>
                 </div>
--- src/components/Services.js
+++ src/components/Services.js
@@ -11,7 +11,7 @@ const Services = () => {
             <h1 className = "py-5">my services</h1>
                 <div className= "container">
                     <div className= "row">
-                        <div className="col-md-3 col--sm-6">
+                        <div className="col-lg-3 col-md-6 col--sm-6">
                             <div className= "box">
                                 <div className="circle"><FontAwesomeIcon className="icon" icon={faTerminal} size='2x'/></div>
 
@@ -20,7 +20,7 @@ const Services = () => {
                             </div>
                         </div>
                         {/* - */}
-                        <div className="col-md-3 col--sm-6">
+                        <div className="col-lg-3 col-md-6 col--sm-6">
                             <div className= "box">
                                 <div className="circle"><FontAwesomeIcon className="icon" icon={faGraduationCap} size='2x'/></div>
 
@@ -29,7 +29,7 @@ const Services = () => {
                             </div>
                         </div>
                         {/* - */}
-                        <div className="col-md-3 col--sm-6">
+                        <div className="col-lg-3 col-md-6 col--sm-6">
                             <div className= "box">
                                 <div className="circle"><FontAwesomeIcon className="icon" icon={faFileCode} size='2x'/></div>
 
@@ -38,7 +38,7 @@ const Services = () => {
                             </div>
                         </div>
                         {/* - */}
-                        <div className="col-md-3 col--sm-6">
+                        <div className="col-lg-3 col-md-6 col--sm-6">
                             <div className= "box">
                                 <div className="circle"><FontAwesomeIcon className="icon" icon={faCogs} size='2x'/></div>
 
